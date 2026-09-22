8e5dc65d7a0e9d9abe21495059b7653a3b8ee842
Tina <Tinatvu04@gmail.com>
2026-09-21 19:52:40 -0500

    Clean up - staring over

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Sidebar from './components/Sidebar';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Links from './components/Links';
-import FAQ from './components/FAQ';
-import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Sidebar />
-        <main className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/links" element={<Links />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="app">
+      <h1>Starting Fresh</h1>
+    </div>
   );
 }
 
